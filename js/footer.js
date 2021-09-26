bd0f3f0202678760eea3a84c02a29bb609c8b3ee
Hyperdriveguy <hyperdriveguy@gmail.com>
2021-09-25 20:22:23 -0600

    Add lesson 2 content

--- js/footer.js
+++ js/footer.js
@@ -1,11 +1,4 @@
-// toLocalDateString
 const options = {year: 'numeric'};
-//document.getElementById('yeet').textContent = new Date().toLocaleDateString('en-US', options);
-
-//document.getElementById('foo').textContent = 'You retarded foobar';
-//let yeet = document.getElementById('lorem').textContent;
-//document.getElementById('lorem').textContent = `Yeety ${ yeet } yeet`;
-
 
 let copyright = document.querySelector('#copyright');
 let modified = document.querySelector('#modified');
@@ -15,4 +8,3 @@ let mod_date = document.lastModified;
 
 modified.innerHTML = `Last Modified: ${mod_date}`;
 copyright.innerHTML = cur_year;
-
