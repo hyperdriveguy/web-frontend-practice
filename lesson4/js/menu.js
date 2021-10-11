5ce1ed1653b41f87d86119090a7b6a0167f323e0
Hyperdriveguy <hyperdriveguy@gmail.com>
2021-10-11 14:53:57 -0600

    additional fine tuning for small display sizes

--- lesson4/js/menu.js
+++ lesson4/js/menu.js
@@ -1,7 +1,22 @@
 const menu_button = document.querySelector('.menu');
-const main_nav = document.querySelector('#main-nav')
+const main_nav = document.querySelector('#main-nav');
 
 menu_button.addEventListener('click', () => {main_nav.classList.toggle('responsive')}, false);
 
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) main_nav.classList.remove('responsive')};
+
+// 
+resize_logo = function() {
+    if (window.innerWidth < 507) {
+        logo_image.src = 'images/logo-compact.png';
+    }
+    else {
+        logo_image.src = 'images/logo-100px.png';
+    }
+}
+
+
+const logo_image = document.querySelector('#logo');
+window.onresize = () => {resize_logo()};
+window.onload = () => {resize_logo()};
