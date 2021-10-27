def6b38b28eb6c61ac27928a01399b85c31b3e20
Hyperdriveguy <hyperdriveguy@gmail.com>
2021-10-27 17:21:52 -0600

    Add lazyloading page

---
+++ lesson-7/js/main.js
@@ -0,0 +1,32 @@
+const preloadableImages = document.querySelectorAll("[data-src]");
+
+function imagePreload(image) {
+    const src = image.getAttribute("data-src");
+
+    if (!src)
+        return;
+
+    image.src = src;
+    image.classList.add("remove-blur");
+}
+
+const imageOptions = {
+    threshold: 0,
+    rootMargin: '0px 0px 400px 0px'
+};
+
+const imageObserver = new IntersectionObserver((entries, imageObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        }
+        else {
+            imagePreload(entry.target);
+            imageObserver.unobserve(entry.target);
+        }
+    })
+}, imageOptions);
+
+preloadableImages.forEach((image) => {
+    imageObserver.observe(image);
+});
